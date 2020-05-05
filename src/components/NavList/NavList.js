8ae2ffaf41d34f0b83f196369e90d3aaa1a1663c
Joshua <joshuakirwin@gmail.com>
2020-05-05 12:53:28 -0500

    bugfixes for list index.

--- src/components/NavList/NavList.js
+++ src/components/NavList/NavList.js
@@ -21,22 +21,29 @@ class NavList extends Component {
     }));
   };
 
-  cycleListWithKeyboard = (e) => {
+  cycleListWithKeyboard = (e, list) => {
     console.log(e.keyCode);
-    const list = this.list.current;
+    console.log(this.state.index);
     //down arrow key
     if (e.keyCode === 40) {
       //
-      this.setState(
-        (prevState) => ({
-          ...prevState,
-          focused: true,
-        }),
-        () => {
-          list.children[this.state.index].firstElementChild.focus();
-          this.incrementDecrementListIndex(this.state.index, true);
-        }
-      );
+      if (
+        list.childElementCount > 0 &&
+        this.state.index < list.childElementCount
+      ) {
+        //
+        this.setState(
+          (prevState) => ({
+            ...prevState,
+            focused: true,
+          }),
+          () => {
+            this.incrementDecrementListIndex(this.state.index, true);
+            list.children[this.state.index].firstElementChild.focus();
+          }
+        );
+      }
+
       //up arrow key
     } else if (e.keyCode === 38) {
       if (this.state.index > 0) {
@@ -46,15 +53,17 @@ class NavList extends Component {
             focused: true,
           }),
           () => {
-            list.children[this.state.index - 1].firstElementChild.focus();
-            this.incrementDecrementListIndex(this.state.index - 1, false);
+            this.incrementDecrementListIndex(this.state.index, false);
+            list.children[this.state.index - 2].firstElementChild.focus();
           }
         );
       }
     } else if (e.keyCode === 9) {
-      this.setState({
+      this.setState((prevState) => ({
+        ...prevState,
         focused: false,
-      });
+        index: 0,
+      }));
     }
   };
 
@@ -65,7 +74,7 @@ class NavList extends Component {
     return (
       <li
         className={`has-drop ${this.state.focused ? "has-drop--focused" : ""}`}
-        onKeyDown={(e) => this.cycleListWithKeyboard(e)}
+        onKeyDown={(e) => this.cycleListWithKeyboard(e, this.list.current)}
         onMouseDown={(e) => this.resetFocus(e)}
       >
         <a href={this.props.href}>{this.props.text}</a>
